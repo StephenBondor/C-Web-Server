5878c45c6a30aea36a9c94b317468de8cad1c13b
Stephen Bondor <bondor@Stephens-MacBook-Pro.local>
2019-05-14 15:17:59 -0700

    MVP part 1 (days 1 and 2)

--- src/server.c
+++ src/server.c
@@ -48,27 +48,49 @@
  * 
  * Return the value from the send() function.
  */
+
+
+// ```
+// HTTP/1.1 200 OK
+// Date: Wed Dec 20 13:05:11 PST 2017
+// Connection: close
+// Content-Length: 41749
+// Content-Type: text/html
+
+// <!DOCTYPE html><html><head><title>Lambda School ...
+// ```
+	
+// 1.1 
 int send_response(int fd, char *header, char *content_type, void *body, int content_length)
 {
     const int max_response_size = 262144;
     char response[max_response_size];
+	time_t gross_time = time(NULL); 
+	struct tm *local_time = localtime(&gross_time);
+    char *timestamp = asctime(local_time);
 
     // Build HTTP response and store it in response
-
-    ///////////////////
-    // IMPLEMENT ME! //
-    ///////////////////
+    int response_length = sprintf(response, 
+		"%s\n"
+        "Content-Type: %s\n"
+        "Content-Length: %d\n"
+        "Connection: close\n"
+		"Date: %s\n",
+		header,
+		content_type,
+        content_length,
+		timestamp
+    );
+	memcpy(response + response_length, body, content_length);  
+	response_length += content_length;
 
     // Send it all!
     int rv = send(fd, response, response_length, 0);
-
     if (rv < 0) {
         perror("send");
     }
-
     return rv;
 }
-
 
 /**
  * Send a /d20 endpoint response
@@ -76,16 +98,13 @@ int send_response(int fd, char *header, char *content_type, void *body, int cont
 void get_d20(int fd)
 {
     // Generate a random number between 1 and 20 inclusive
-    
-    ///////////////////
-    // IMPLEMENT ME! //
-    ///////////////////
+	srand(time(NULL));   
+	int r = rand() % 20; 
+	char result[8];
+	sprintf(result, "%d", r);   
 
     // Use send_response() to send it back as text/plain data
-
-    ///////////////////
-    // IMPLEMENT ME! //
-    ///////////////////
+	send_response(fd, "HTTP/1.1 200 OK", "text/plain", result, strlen(result));
 }
 
 /**
@@ -122,6 +141,25 @@ void get_file(int fd, struct cache *cache, char *request_path)
     ///////////////////
     // IMPLEMENT ME! //
     ///////////////////
+	char filepath[4096];
+    struct file_data *filedata; 
+    char *mime_type;
+
+    // Fetch the file
+    snprintf(filepath, sizeof filepath, "%s""%s", SERVER_ROOT, request_path);
+    filedata = file_load(filepath);
+
+    if (filedata == NULL) 
+	{
+        resp_404(fd);
+	    file_free(filedata);
+    } 
+	else 
+	{
+		mime_type = mime_type_get(filepath);
+		send_response(fd, "HTTP/1.1 200 OK", mime_type, filedata->data, filedata->size);
+	}
+
 }
 
 /**
@@ -140,33 +178,46 @@ char *find_start_of_body(char *header)
 /**
  * Handle HTTP request and send response
  */
+ // 1.2
 void handle_http_request(int fd, struct cache *cache)
 {
     const int request_buffer_size = 65536; // 64K
     char request[request_buffer_size];
+	char method[8];
+    char path[8192];
 
     // Read request
     int bytes_recvd = recv(fd, request, request_buffer_size - 1, 0);
-
     if (bytes_recvd < 0) {
         perror("recv");
         return;
     }
 
-
-    ///////////////////
-    // IMPLEMENT ME! //
-    ///////////////////
-
     // Read the first two components of the first line of the request 
+    sscanf(request, "%s %s", method, path);
+    printf("method: \"%s\"\n", method);
+    printf("path: \"%s\"\n", path);
  
     // If GET, handle the get endpoints
-
-    //    Check if it's /d20 and handle that special case
-    //    Otherwise serve the requested file by calling get_file()
-
-
-    // (Stretch) If POST, handle the post request
+	if (strcmp(method, "GET") == 0)
+	{
+		//    Check if it's /d20 and handle that special case
+		if (strcmp(path, "/d20") == 0)
+		{
+			printf("hit d20\n");
+			get_d20(fd);
+		} 
+		else 
+		{
+			//  Otherwise serve the requested file by calling get_file()
+			get_file(fd, cache, path);
+		}
+	} 
+	else 
+	{
+    	// (Stretch) If POST, handle the post request
+		printf("get rekt");
+	}
 }
 
 /**
@@ -213,7 +264,7 @@ int main(void)
         
         // newfd is a new socket descriptor for the new connection.
         // listenfd is still listening for new connections.
-
+		// resp_404(newfd);
         handle_http_request(newfd, cache);
 
         close(newfd);
